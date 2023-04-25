af1bc383275b84433facd1d27e16d8980e891b6c
Jack Barry <jack.barry@live.com>
2023-04-25 08:46:43 +0100

    tree refinement

--- webapp/views.py
+++ webapp/views.py
@@ -5,25 +5,20 @@ import json
 #create a json rep of the file system 
 def create_fs_tree(path):
     if os.path.isfile(path):
-        return {'id': path, 'text': os.path.basename(path)}
+        return {'id': path, 'text': os.path.basename(path), 'type': 'file'}
     elif os.path.isdir(path):
         children = []
         for fname in os.listdir(path):
             child_path = os.path.join(path, fname)
             children.append(create_fs_tree(child_path))
-        return {'id': path, 'text': os.path.basename(path), 'children': children}
+        return {'id': path, 'text': os.path.basename(path), 'type': 'folder', 'children': children}
 
-# Create your views here.
-#create index view which displays the index.html template
 def index(request):
-    # tree = []
-    # for root, dirnames, filenames in os.walk('.'):
-    #     for fname in filenames:
-    #         tree.append(create_fs_tree(os.path.join(root, fname)))
-    #     for dirname in dirnames:
-    #         tree.append(create_fs_tree(os.path.join(root, dirname)))
-    tree = create_fs_tree('.')
-    files_json = json.dumps([tree])
+    tree = []
+    #iterate over the directory "." and call create_fs_tree on ech file and folder within the directory
+    for fname in os.listdir('.'):
+        tree.append(create_fs_tree(fname))
+    files_json = json.dumps(tree)
 
     return render(request, 'index.html', {'files': files_json})
 
